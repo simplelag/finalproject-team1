0596830017b70b58a1a87e91b417e984adf26720
simplelag <ioia33@Naver.com>
2023-08-01 15:22:16 +0900

    react+spring boot 병합 최종 완료

--- finalproject/src/main/java/com/bitc/finalproject/controller/TestController.java
+++ finalproject/src/main/java/com/bitc/finalproject/controller/TestController.java
@@ -1,14 +1,12 @@
 package com.bitc.finalproject.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("http://localhost:3000")
 @RestController
 public class TestController {
-    @GetMapping("/hello")
-    public String test(){
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String hello() throws Exception{
         return "Hello World!";
     }
 }
