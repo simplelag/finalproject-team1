86745440f6212fca8ad602d9cbc2f846a63aabf1
simplelag <ioia33@Naver.com>
2023-08-01 10:21:13 +0900

    기본 틀 생성

---
+++ finalproject/src/main/java/com/bitc/finalproject/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.bitc.finalproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/api/hello")
+    public String test(){
+        return "Hello World!";
+    }
+}
---
+++ finalproject_react/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import {useEffect, useState} from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch('/api/hello')
+        .then(response => response.text())
+        .then(message => {
+          setMessage(message);
+        });
+  },[])
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+       <h1 className="App-title">{message}</h1>
+      </header>
+      <p>
+        To get started, edit <code>src/App.js</code> and save to reload
+      </p>
+    </div>
+  );
+}
+
+export default App;
