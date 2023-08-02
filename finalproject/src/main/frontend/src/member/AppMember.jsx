738b0251889a808e39901d645445d016c27e0509
DESKTOP-CG76282\junseong <sjsnok35@gmail.com>
2023-08-02 16:51:49 +0900

    0802(수) 통합에 react front 설정(지금 당장은 통합이 어려움)

---
+++ finalproject/src/main/frontend/src/member/AppMember.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import JoinMember from "./JoinMember";
+import Join from "./Join";
+
+function AppMember(props) {
+
+    return (
+        <div className={'container'}>
+            <BrowserRouter>
+                <Routes>
+                    <Route>
+                        <Route index element={<Join />}></Route>
+                        <Route path={'/join'} element={<JoinMember />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
+}
+export default AppMember;
