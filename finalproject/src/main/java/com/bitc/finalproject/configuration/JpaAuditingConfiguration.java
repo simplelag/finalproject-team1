207b5301a3e078475b10a017cda68734b9c2c3f7
sberrycup <jhyoon315@gmail.com>
2023-08-02 11:02:11 +0900

    데이터베이스 연결 확인 완료

---
+++ finalproject/src/main/java/com/bitc/finalproject/configuration/JpaAuditingConfiguration.java
@@ -0,0 +1,9 @@
+package com.bitc.finalproject.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+}
