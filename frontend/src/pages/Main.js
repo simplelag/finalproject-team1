6ffdd8b368c647a8ffd4a75e58e382d0c7234a2c
gidul0491 <pol0258@naver.com>
2023-08-02 18:12:27 +0900

    리액트에서 클라이언트 id 확인하기 - backend controller에음 어떻게하는지 설명 써놨음

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -3,9 +3,10 @@ import axios from "axios";
 
 function Main(props) {
     const apiTest = () => {
-        axios.get('http://localhost:8080/main.api')
+        axios.get('/api/main',{ withCredentials: true })
             .then(res => {
-                alert(`수신된 데이터 : ${res.data}`);
+                sessionStorage.setItem("id",res.data.id);
+                alert(`수신된 데이터 : ${res.data.content}, id : ${res.data.id}`);
             })
             .catch(err => {
                     alert(err);
@@ -16,7 +17,7 @@ function Main(props) {
     return (
         <div>
             <p>mainPage</p>
-            <button type={"button"} className={'btn btn-primary me-3'} onClick={apiTest}>main.api</button>
+            <button type={"button"} className={'btn btn-outline-primary me-3'} onClick={apiTest}>main.api</button>
         </div>
     );
 }
