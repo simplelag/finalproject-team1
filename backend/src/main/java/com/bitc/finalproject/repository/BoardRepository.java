e380a265b2c31d3ddec0968b3f0ea909770f6135
gidul0491 <pol0258@naver.com>
2023-08-09 15:47:25 +0900

    no message

--- backend/src/main/java/com/bitc/finalproject/repository/BoardRepository.java
+++ backend/src/main/java/com/bitc/finalproject/repository/BoardRepository.java
@@ -10,8 +10,8 @@ import java.util.List;
 
 public interface BoardRepository extends JpaRepository<BoardEntity, Integer> {
     List<BoardEntity> findAllByOrderByBoardPkDesc();
-    @Query("SELECT boardPk, boardTitle, boardWriterName, boardCategory, boardDatetime, boardVisitCount, count(c.commentPk) as commentNumber FROM board b left join comment c on b.boardPk = c.commentBoardPk
-            where boardCategory = \"관리자문의\" group by b.boardPk")
+//    @Query("SELECT boardPk, boardTitle, boardWriterName, boardCategory, boardDatetime, boardVisitCount, count(c.commentPk) as commentNumber FROM board b left join comment c on b.boardPk = c.commentBoardPk
+//            where boardCategory = \"관리자문의\" group by b.boardPk")
     
     List<BoardEntity> findAllByBoardCategory(String boardCategory, Pageable pageable);
     int countByBoardCategory(String boardCategory);
