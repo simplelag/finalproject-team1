5d10b707a03d93d09a8eec9a4bee6a8fcb8426a8
gidul0491 <pol0258@naver.com>
2023-08-09 15:14:57 +0900

    no message

--- backend/src/main/java/com/bitc/finalproject/repository/BoardRepository.java
+++ backend/src/main/java/com/bitc/finalproject/repository/BoardRepository.java
@@ -3,12 +3,16 @@ package com.bitc.finalproject.repository;
 import com.bitc.finalproject.entity.BoardEntity;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 
 import java.util.List;
 
 public interface BoardRepository extends JpaRepository<BoardEntity, Integer> {
     List<BoardEntity> findAllByOrderByBoardPkDesc();
+    @Query("SELECT boardPk, boardTitle, boardWriterName, boardCategory, boardDatetime, boardVisitCount, count(c.commentPk) as commentNumber FROM board b left join comment c on b.boardPk = c.commentBoardPk
+            where boardCategory = \"관리자문의\" group by b.boardPk")
+    
     List<BoardEntity> findAllByBoardCategory(String boardCategory, Pageable pageable);
     int countByBoardCategory(String boardCategory);
 }
