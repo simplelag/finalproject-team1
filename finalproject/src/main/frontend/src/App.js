2abb147b72fb2a2addac4b9eb16ef0058abb0c9c
sberrycup <jhyoon315@gmail.com>
2023-08-17 09:00:16 +0900

    no message

--- finalproject/src/main/frontend/src/App.js
+++ finalproject/src/main/frontend/src/App.js
@@ -2,21 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
 import MainHome from "./MainHome";
+import Header from "./Header";
+import Navbar from "./";
 
 function App() {
   return (
     <div>
-      <button type={'button'}onClick={() =>{
-          axios.get('http://localhost:8080/hello')
-              .then(res =>{
-                  alert(`통신성공\n${res.data}`);
-              })
-              .catch(err =>{
-                  alert('통신 실패');
-              })
-      }}>확인</button>
-
-        <MainHome />
+        <Header />
+        <Navbar />
     </div>
   );
 }
--- finalproject/src/main/frontend/src/MainHome.js
+++ finalproject/src/main/frontend/src/MainHome.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function MainHome(props) {
     return (
-        <div>
+        <div className={'container'}>
             <h1>test</h1>
         </div>
     )
